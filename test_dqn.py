65022fcce32a89b09c80cf9b77677a1b3d65f92f
Eoin Pinaqui <pinaquie@tcd.ie>
2023-03-05 20:30:01 +0000

    removed unnecessary change

--- test_dqn.py
+++ test_dqn.py
@@ -9,7 +9,7 @@ import json
 print('Here')
 
 env = SinglePlayerGame()
-agent = Agent(lr=0.001, gamma=0.95, n_actions=env.action_space.n, epsilon=0.0, epsilon_end=0.0, epsilon_dec=1e-4, batch_size=64, linear=True)
+agent = Agent(lr=0.001, gamma=0.95, n_actions=env.action_space.n, epsilon=0.1, epsilon_end=0.1, epsilon_dec=1e-4, batch_size=64, linear=True)
 agent.load_model()
 
 for i in range(100):
